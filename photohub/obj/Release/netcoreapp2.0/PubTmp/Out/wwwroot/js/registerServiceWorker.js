886bf2cc188750514c210ca607bedc6c6e16d0ba
maxa1m <max.mironenko3@gmail.com>
2018-11-10 10:35:49 +0200

    Remove redundant files.

--- photohub/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/registerServiceWorker.js
+++
@@ -1,8 +0,0 @@
-﻿/*  Register service worker  */
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(() => navigator.serviceWorker.ready.then((worker) => {
-            worker.sync.register('syncdata');
-        }))
-        .catch((err) => console.log(err));
-}
